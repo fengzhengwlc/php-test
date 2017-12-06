aa4142994a44778fc476ec56a44e3f0a5a19d264
Lemine <84722742@qq.com>
2017-12-06 10:23:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
 <?php
-echo "<H1>Hello World</H1>";
+echo "<H1>again</H1>";
 echo phpversion(); 
-echo "<H1>Hello again</h1>";
-echo "<p>again</p>";
-echo "<p>who</p>";
-echo "<p>改变了</p>";
-echo "<h1>TEST118</h1>";
-echo "<H1>Hello World</H1>";
-echo phpversion(); 
-echo "<H1>Hello again</h1>";
-echo "<p>again</p>";
-echo "<p>who</p>";
-echo "<p>改变了</p>";
-echo "<h1>TEST118</h1>";
 ?>
