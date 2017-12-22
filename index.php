c0c2eb119801cabab570ff8998542591fac8989b
Lemine <84722742@qq.com>
2017-12-22 15:30:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
 <?php
-echo "<H1>Hello World</H1>";
-echo phpversion(); 
-echo "<H1>Hello again</h1>";
-echo "<p>again</p>";
-echo "<p>who</p>";
-echo "<p>改变了</p>";
-echo "<h1>TEST118</h1>";
-echo "<H1>Hello World</H1>";
-echo phpversion(); 
-echo "<H1>Hello again</h1>";
-echo "<p>again</p>";
-echo "<p>who</p>";
-echo "<p>改变了</p>";
-echo "<h1>TEST118</h1>";
-echo "<b>Is webhook ready?</b>";
-echo "<b>Is update ready?</b>";
-echo "<h1>Is update ready?</h1>";
-echo "<h1>十二月十八?</h1>";
+echo "<H1>HELLO WORLD</H1>";
 ?>
